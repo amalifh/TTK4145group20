b67485e7321144261b70740ed5583571603434ca
GingerNinju <aukner.a@gmail.com>
2025-01-20 09:33:31 +0100

    changes

---
+++ Øving 2/UDP/Send/UDPSend2.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+func UDPsend() {
+	addr, _ := net.ResolveUDPAddr("udp", "10.100.23.204:20020")
+	conn, _ := net.DialUDP("udp", nil, addr)
+	conn.Write([]byte("Hello from spot 22"))
+	time.Sleep(1500 * time.Millisecond)
+}
+
+func UDPreceive() {
+	udpAddr, err := net.ResolveUDPAddr("udp", ":30000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(1)
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(2)
+	//defer conn.Close()
+	//buf := make([]byte, 1024)
+	for {
+		buf := make([]byte, 1024)
+		n, addr, err := conn.ReadFromUDP(buf)
+		fmt.Println(3)
+		if err != nil {
+			fmt.Println(err)
+
+			return
+		}
+		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+
+	}
+}
+
+func main() {
+
+	go UDPsend()
+	go UDPreceive()
+
+	time.Sleep(5000 * time.Millisecond)
+
+}
