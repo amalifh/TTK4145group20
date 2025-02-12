d27cd420c94f54f0b569bf88746734c444984ac7
GingerNinju <aukner.a@gmail.com>
2025-02-12 16:41:20 +0100

    Ex4 UDP working

--- Exercise 4/UDP/main.go
+++ Exercise 4/UDP/main.go
@@ -9,59 +9,53 @@ import (
 	"time"
 )
 
-func Primary() {
+func Primary(lastValue int) {
 	addr, _ := net.ResolveUDPAddr("udp", "localhost:20022")
 	conn, _ := net.DialUDP("udp", nil, addr)
-	exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()
+	//exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()            // Linux
+	exec.Command("cmd", "/C", "start", "powershell", "go", "run", "main.go").Run() // Windows
 	var str string
-	lastValue := Backup()
-	for{
+
+	for {
 		lastValue++
 		str = strconv.Itoa(int(lastValue))
 		data := []byte(str)
 		conn.Write(data)
 		fmt.Println(lastValue)
-		time.Sleep(1500 * time.Millisecond)		
+		time.Sleep(1500 * time.Millisecond)
 	}
-	
+
 }
 
 func Backup() int {
-	udpAddr, err := net.ResolveUDPAddr("udp", ":20022")
-	if err != nil {
-		return 0
-	}
+	udpAddr, _ := net.ResolveUDPAddr("udp", ":20022")
+	conn, _ := net.ListenUDP("udp", udpAddr)
 
-	conn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		fmt.Println(err)
-		return 0 
-	}
 	fmt.Println("Backup connected")
 	defer conn.Close()
+	var num int
 	for {
 		buf := make([]byte, 1024)
 		conn.SetDeadline(time.Now().Add(3 * time.Second))
 		value, err := conn.Read(buf)
-		data := buf[0:value]
-		strdata := string(data)
-		num, _:= strconv.Atoi(strdata)
-		fmt.Println(num)
-		if err != nil{
-			fmt.Println(err)
+		if err != nil {
 			fmt.Println("Sending num:", num)
 			return num
+		} else {
+			data := buf[0:value]
+			strdata := string(data)
+			num, _ = strconv.Atoi(strdata)
+			fmt.Println(num)
 		}
 	}
 }
 
 func main() {
-	//var lastValue int
 	var wg sync.WaitGroup
-
 	wg.Add(1)
-	//fmt.Println("LastValue: ", lastValue)
-	Primary()
+	lastValue := Backup()
+	fmt.Println("LastValue: ", lastValue)
+	Primary(lastValue)
 	wg.Wait()
-	
+
 }
