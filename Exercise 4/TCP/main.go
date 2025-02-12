88437c14ddb4660507a9d3f8ba5a95d90d79121c
GingerNinju <aukner.a@gmail.com>
2025-02-12 17:16:28 +0100

    Ex4 TCP working

--- Exercise 4/TCP/main.go
+++ Exercise 4/TCP/main.go
@@ -9,49 +9,59 @@ import (
 	"time"
 )
 
-func Primary(i int) {
-	addr, _ := net.ResolveTCPAddr("tcp", "localhost:20022")
-	conn, _ := net.DialTCP("tcp", nil, addr)
-	exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()
+func Primary(lastValue int) {
+	fmt.Println("Primary initializing")
+	ln, _ := net.Listen("tcp", ":20022")
+	//exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()            // Linux
+	exec.Command("cmd", "/C", "start", "powershell", "go", "run", "main.go").Run() // Windows
+	fmt.Println("Primary listening")
+	conn, _ := ln.Accept()
+	fmt.Println("Primary connected")
 
-	for{
-		i++
-		str := strconv.Itoa(i)
-		conn.Write([]byte(str))
-		fmt.Println(i)
-		time.Sleep(1500 * time.Millisecond)		
+	var str string
+	for {
+		lastValue++
+		str = strconv.Itoa(int(lastValue))
+		data := []byte(str)
+		conn.Write(data)
+		fmt.Println(lastValue)
+		time.Sleep(1500 * time.Millisecond)
 	}
 }
 
 func Backup() int {
-	ln, _ := net.Listen("tcp", ":20022")
-	conn, _ := ln.Accept()
-	fmt.Println("Backup connected")
+	fmt.Println("Backup initializing")
+	addr, _ := net.ResolveTCPAddr("tcp", "localhost:20022")
+	fmt.Println("Backup resolved")
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		fmt.Println("Error dialing")
+		return 0
+	}
+	fmt.Println("Backup dialed")
 
-	defer conn.Close()
+	var num int
 	for {
 		buf := make([]byte, 1024)
-		conn.SetDeadline(time.Now().Add(3 * time.Second))
-		_, err := conn.Read(buf)
-		if err != nil{
-			//fmt.Println(err)
-			return 0
+		value, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Sending num:", num)
+			conn.Close()
+			return num
+		} else {
+			data := buf[0:value]
+			strdata := string(data)
+			num, _ = strconv.Atoi(strdata)
+			fmt.Println(num)
 		}
 	}
 }
 
-
 func main() {
 	var wg sync.WaitGroup
 	lastValue := 0
-
-
 	wg.Add(1)
 	lastValue = Backup()
 	Primary(lastValue)
 	wg.Wait()
-	
-	
-
 }
-
