a438bbf07896b00b146799024f71c7fed67e96ef
GingerNinju <aukner.a@gmail.com>
2025-02-19 20:46:29 +0100

    TCP first steps

--- driver-go/network/network.go
+++ driver-go/network/network.go
@@ -1,1 +1,16 @@
 package network
+
+func TCPNetworkInit() {
+	// Initialize the TCP network
+
+}
+
+func TCPNetworkSend() {
+	// Send a message over the TCP network
+
+}
+
+func TCPNetworkReceive() {
+	// Receive a message over the TCP network
+
+}
