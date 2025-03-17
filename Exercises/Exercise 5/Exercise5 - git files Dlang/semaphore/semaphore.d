91fa134d7391d9ebb722e098a0f641304967e0f8
GingerNinju <aukner.a@gmail.com>
2025-03-17 10:30:22 +0100

    Semaphore done

--- Exercises/Exercise 5/Exercise5 - git files Dlang/semaphore/semaphore.d
+++ Exercises/Exercise 5/Exercise5 - git files Dlang/semaphore/semaphore.d
@@ -33,29 +33,29 @@ class Resource(T) {
     }
     
     T allocate(int priority){
-         mtx.wait();
-    if(busy){
-        numWaiting[priority]++;
-        mtx.notify();
-        sems[priority].wait();
-        numWaiting[priority]--;
-    }  else {
-        mtx.notify();
-    }
-    busy = true;
-    return value;
+        mtx.wait();
+        if(busy){
+            numWaiting[priority]++;
+            mtx.notify();
+            sems[priority].wait();
+            numWaiting[priority]--;
+        }  else {
+            mtx.notify();
+        }
+        busy = true;
+        return value;
     }
     
     void deallocate(T v){
         mtx.wait();
-    busy = false;
-    if(numWaiting[1] > 0){
-        sems[1].notify();
-    } else if(numWaiting[0] > 0) {
-        sems[0].notify();
-    } 
-     mtx.notify();
-     value = v;
+        busy = false;
+        if(numWaiting[1] > 0){
+            sems[1].notify();
+        } else if(numWaiting[0] > 0) {
+            sems[0].notify();
+        } 
+        mtx.notify();
+        value = v;
     }
 }
 
