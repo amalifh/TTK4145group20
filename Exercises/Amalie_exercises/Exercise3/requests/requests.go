c17b819c07e63267fd935e133bc62621dc8e546d
amalifh <amalifh@stud.ntnu.no>
2025-03-22 13:47:12 +0100

    just rewrote

--- Exercises/Amalie_exercises/Exercise3/requests/requests.go
+++ Exercises/Amalie_exercises/Exercise3/requests/requests.go
@@ -11,28 +11,31 @@ type Dirn_behaviour_pair struct {
 }
 
 func requests_above(e types.Elevator) bool {
-	for f := e.E_floor + 1; f < types.N_floors; f++ {
+	//checking of there are any requests above current floor
+	for floor = e.E_floor+1; floor < types.N_floors; floor++{
+		//if there are any requests that means that one of the three buttons has been pressed
 		for btn := 0; btn < types.N_buttons; btn++ {
-			if e.E_requests[f][btn] {
+			if e.E_requests[floor][btn] {
 				return true
 			}
 		}
 	}
 	return false
 }
+
 
 func requests_below(e types.Elevator) bool {
-	for f := 0; f < e.E_floor; f++ {
-		for btn := 0; btn < types.N_buttons; btn++ {
-			if e.E_requests[f][btn] {
+	for floor := 0; floor < e.E_floor; floor++ {
+		for btn := 0; btn < types.N_buttons; btn ++ {
+			if e.E_requests[floor][btn]{
 				return true
 			}
 		}
-	}
+	} 
 	return false
 }
 
-func requests_here(e types.Elevator) bool {
+func requests_here(e types.Elevator) bool{
 	for btn := 0; btn < types.N_buttons; btn++ {
 		if e.E_requests[e.E_floor][btn] {
 			return true
@@ -41,6 +44,7 @@ func requests_here(e types.Elevator) bool {
 	return false
 }
 
+func requests_chooseDirn(e types.Elevator)
 // think about what makes sense to check first, depending on the direction the elevator is moving in
 func requests_chooseDirn(e types.Elevator) Dirn_behaviour_pair {
 	switch e.E_dirn {
@@ -100,29 +104,5 @@ func requests_shouldClearImmediately(e types.Elevator, btn_floor int, btn_type t
 }
 
 func requests_clearAtCurrentFloor(e types.Elevator) types.Elevator {
-	switch e.E_config.ClearRequestVariant {
-	case types.CV_All:
-		for btn := 0; btn < types.N_buttons; btn++ {
-			e.E_requests[e.E_floor][btn] = false
-		}
-	case types.CV_InDirn:
-		e.E_requests[e.E_floor][types.B_Cab] = false
-		switch e.E_dirn {
-		case types.Dirn_UP:
-			if !requests_above(e) && !e.E_requests[e.E_floor][types.B_HallUp] {
-				e.E_requests[e.E_floor][types.B_HallDown] = false
-			}
-		case types.Dirn_Down:
-			if !requests_below(e) && !e.E_requests[e.E_floor][types.B_HallDown] {
-				e.E_requests[e.E_floor][types.B_HallUp] = false
-			}
-		case types.Dirn_Stop:
-			fallthrough
-
-		default: //clears both as deafult
-			e.E_requests[e.E_floor][types.B_HallDown] = false
-			e.E_requests[e.E_floor][types.B_HallUp] = false
-		}
-	}
-	return e
+	switch 
 }
